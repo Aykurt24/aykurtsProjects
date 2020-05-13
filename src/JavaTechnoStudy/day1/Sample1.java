37e9d5f861dfa8ce00409a9cd5cd7c34a216007f
Aykurt24 <doganaykurt@gmail.com>
2020-05-13 01:17:20 -0400

    Update

--- src/JavaTechnoStudy/day1/Sample1.java
+++ src/JavaTechnoStudy/day1/Sample1.java
@@ -5,5 +5,9 @@ public class Sample1 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!! ");
+        System.out.println("Dogan ");
+        System.out.println("Aykurt ");
+
+
     }
 }
